430f3ae87982a2b84093d14b7e2d746e24cc1ac7
rudy <mrj9990123@gmail.com>
2019-08-26 09:48:29 +1000

    Successfully navigate to item list page after click on a category image.

--- app/src/main/java/com/example/FamilyRegister/CategoryAdapter.kt
+++ app/src/main/java/com/example/FamilyRegister/CategoryAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.FamilyRegister
 
 import android.content.Context
-import android.view.View
-import android.view.ViewGroup
+import android.content.Intent
+import android.util.Log
+import android.view.*
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 /**
  * @author: Renjie Meng
@@ -13,19 +17,89 @@ import androidx.recyclerview.widget.RecyclerView
 class CategoryAdapter(val items: ArrayList<Upload>, val mContext: Context) :
     RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
 
+    var listener: OnItemClickerListener? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryAdapter.CategoryViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val v = LayoutInflater.from(mContext).inflate(R.layout.image_item, parent, false)
+        return CategoryViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        val currUpload = items[position]
+        holder.textViewName.text = currUpload.name
+        // Load image to ImageView via its URL from Firebase Storage
+        Picasso.get()
+            .load(currUpload.url)
+            .placeholder(R.mipmap.ic_launcher)
+            .fit()
+            .centerCrop()
+            .into(holder.imageView)
+        Log.d("url", currUpload.url)
+        holder.imageView.setOnClickListener {
+
+            val goToCategory = Intent(mContext, ImagesActivity::class.java)
+            mContext.startActivity(goToCategory)
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return items.size
     }
 
-    override fun onBindViewHolder(holder: CategoryAdapter.CategoryViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    interface OnItemClickerListener {
+        fun onItemClick(position: Int)
+        fun onWhatEverClick(position: Int)
+        fun onDeleteClick(position: Int)
     }
 
-    inner class CategoryViewHolder(val viewItem: View) : RecyclerView.ViewHolder(viewItem){
+    inner class CategoryViewHolder(val viewItem: View) : RecyclerView.ViewHolder(viewItem), View.OnClickListener,
+        View.OnCreateContextMenuListener, MenuItem.OnMenuItemClickListener {
+        val textViewName: TextView = viewItem.findViewById(R.id.txt_name)
+        val imageView: ImageView = viewItem.findViewById(R.id.img_upload)
+
+        init {
+            viewItem.setOnClickListener(this)
+            viewItem.setOnCreateContextMenuListener(this)
+        }
+
+        override fun onMenuItemClick(p0: MenuItem?): Boolean {
+            if (listener != null) {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    when (p0?.itemId) {
+                        1 -> {
+                            listener!!.onWhatEverClick(position)
+                            return true
+                        }
+                        2 -> {
+                            listener!!.onDeleteClick(position)
+                            return true
+                        }
+                    }
+                }
+            }
+            return false
+        }
+
+        override fun onCreateContextMenu(p0: ContextMenu?, p1: View?, p2: ContextMenu.ContextMenuInfo?) {
+            p0?.setHeaderTitle("Select Action")
+            val doWatEver = p0?.add(Menu.NONE, 1, 1, "Do Whatever: ")
+            val delete = p0?.add(Menu.NONE, 2, 2, "Do Delete: ")
+
+            doWatEver?.setOnMenuItemClickListener(this)
+
+            delete?.setOnMenuItemClickListener(this)
+        }
+
+        override fun onClick(p0: View?) {
+            if (listener != null) {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    listener!!.onItemClick(position)
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/FamilyRegister/CategoryFragment.kt
+++ app/src/main/java/com/example/FamilyRegister/CategoryFragment.kt
@@ -1,19 +1,36 @@
 package com.example.FamilyRegister
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresPermission
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_category.view.*
 import kotlinx.android.synthetic.main.fragment_register.*
 
-class CategoryFragment : Fragment() {
+class CategoryFragment() : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 
         // Snippet from "Navigate to the next Fragment" section goes here.
 
         val view = inflater.inflate(R.layout.fragment_category, container, false)
+
+        // retrieve all the category data from the database
+        val categories = ArrayList<Upload>()
+        // put them into category objects
+        RegisterFragment.defaultCategories.forEach{
+            val category = Upload(it, R.drawable.furniture_default.toString())
+            categories.add(category)
+        }
+        // set it into the adapter
+        val categoryAdapter = CategoryAdapter(categories, context!!)
+        view.category_recycler_view.layoutManager = GridLayoutManager(activity, 2)
+        view.category_recycler_view.adapter = categoryAdapter
 
         return view
     }
