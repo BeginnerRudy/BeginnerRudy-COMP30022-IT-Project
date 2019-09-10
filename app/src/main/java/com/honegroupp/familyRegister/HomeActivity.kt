ff84982da37b302c8641b311b09a174a6b8fc8e8
Jaycco <z845050262@gmail.com>
2019-09-10 22:47:57 +1000

    1. changef package name 2. add layout resource directory

--- app/src/main/java/com/honegroupp/familyRegister/HomeActivity.kt
+++ app/src/main/java/com/honegroupp/familyRegister/HomeActivity.kt
@@ -1,7 +1,13 @@
-package com.honegroupp.familyRegister
+package com.honegroupp.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.honegroupp.familyRegister.R
 
-class HomeActivity: AppCompatActivity(){
+class HomeActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
 }
