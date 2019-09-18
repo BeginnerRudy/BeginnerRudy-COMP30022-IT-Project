61819030587fe0e9ef29fc2fd7a26442eaa95600
rudy <mrj9990123@gmail.com>
2019-09-18 16:04:55 +1000

    Finish basic join family functionality

--- app/src/main/java/com/honegroupp/familyRegister/view/family/FamilyJoinActivity.kt
+++ app/src/main/java/com/honegroupp/familyRegister/view/family/FamilyJoinActivity.kt
@@ -1,21 +1,39 @@
 package com.honegroupp.familyRegister.view.family
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.honegroupp.familyRegister.R
+import com.honegroupp.familyRegister.controller.FamilyController
+import kotlinx.android.synthetic.main.create_family_main.*
+import kotlinx.android.synthetic.main.join_family_main.*
 
-class FamilyJoinActivity: AppCompatActivity(){
+
+class FamilyJoinActivity : AppCompatActivity() {
+    lateinit var currUid: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.join_family_main)
 
+        //get User ID
+        currUid = intent.getStringExtra("UserID")
+
+
         // TODO whether the user could join a family
 
-        // search for the family
+        joinFamily(familyJoinConfirm)
+    }
 
-        // check password for the family
-
-        // join the family
+    private fun joinFamily(button: Button) {
+        button.setOnClickListener {
+            FamilyController.validateJoinFamilyInput(
+                this,
+                edit_text_family_name_input,
+                edit_text_family_password_input,
+                currUid
+            )
+        }
     }
 }
-
