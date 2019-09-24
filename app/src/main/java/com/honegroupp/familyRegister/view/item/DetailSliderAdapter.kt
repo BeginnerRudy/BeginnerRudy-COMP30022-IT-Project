a367a3dcca2e0d43e6dcf66d4c5c9583fee98b6d
Jaycco <z845050262@gmail.com>
2019-09-24 21:34:02 +1000

    item detail sliders adapter comments

--- app/src/main/java/com/honegroupp/familyRegister/view/item/DetailSliderAdapter.kt
+++ app/src/main/java/com/honegroupp/familyRegister/view/item/DetailSliderAdapter.kt
@@ -14,7 +14,7 @@ import com.honegroupp.familyRegister.model.Item
 import com.squareup.picasso.Picasso
 
 class DetailSliderAdapter(val items: ArrayList<Item>, val context: Context) : PagerAdapter() {
-    var listener: DetailSliderAdapter.OnItemClickerListener? = null
+    var listener: OnItemClickerListener? = null
 
     interface OnItemClickerListener {
         fun onItemClick(position: Int, items:ArrayList<Item>)
@@ -23,7 +23,6 @@ class DetailSliderAdapter(val items: ArrayList<Item>, val context: Context) : Pa
     }
 
     override fun getCount(): Int {
-        Log.d("detailitemssize", items.size.toString())
         return items.size
     }
 
@@ -31,74 +30,50 @@ class DetailSliderAdapter(val items: ArrayList<Item>, val context: Context) : Pa
         return view == `object`
     }
 
-
     override fun instantiateItem(container: ViewGroup, position: Int): View {
-        var layoutInflater:LayoutInflater = LayoutInflater.from(context)
+        val layoutInflater:LayoutInflater = LayoutInflater.from(context)
         val view: View = layoutInflater.inflate(R.layout.slide_detail_layout, container, false)
 
-        var slideImageView = view.findViewById<ImageView>(R.id.detail_image)
-        var slideHeaing = view.findViewById<TextView>(R.id.detail_heading)
-        var slideDescription = view.findViewById<TextView>(R.id.detail_desc)
+        val slideImageView = view.findViewById<ImageView>(R.id.detail_image)
+        val slideHeaing = view.findViewById<TextView>(R.id.detail_heading)
+        val slideDescription = view.findViewById<TextView>(R.id.detail_desc)
 
-        val currUpload = items[position]
+        val currItemUploads = items[position]
 
 
         // Load image to ImageView via its URL from Firebase Storage
         Picasso.get()
-            .load(currUpload.imageURLs[0])
+            .load(currItemUploads.imageURLs[0])
             .placeholder(R.mipmap.ic_launcher)
             .into(slideImageView)
-        Log.d("url", currUpload.imageURLs[0])
-        slideHeaing.setText(currUpload.itemName)
-        slideDescription.setText(currUpload.itemDescription)
+        slideHeaing.setText(currItemUploads.itemName)
+        slideDescription.setText(currItemUploads.itemDescription)
 
         view.findViewById<Button>(R.id.detail_edit).setOnClickListener{
             val intent = Intent(context, ItemEdit::class.java)
             context.startActivity(intent)
         }
 
+        // set on click listeners
         view.findViewById<ImageView>(R.id.detail_image).setOnClickListener{
-            Log.d("detailonitemclick",position.toString())
             listener!!.onItemClick(position, items)
         }
 
         view.findViewById<Button>(R.id.detail_download).setOnClickListener{
-            Log.d("detaildowloding",position.toString())
             listener!!.onDownloadClick(position, items)
         }
 
         view.findViewById<Button>(R.id.detail_share).setOnClickListener{
-            Log.d("detailsharing",position.toString())
             Picasso.get()
-                .load(currUpload.imageURLs[0])
+                .load(currItemUploads.imageURLs[0])
                 .placeholder(R.mipmap.ic_launcher)
                 .into(slideImageView)
             listener!!.onShareClick(position, items, slideImageView)
         }
+
         container.addView(view)
         return view
     }
-
-    inner class ImageViewHolder(val viewItem: View) : View.OnClickListener,
-        View.OnCreateContextMenuListener, MenuItem.OnMenuItemClickListener {
-        override fun onMenuItemClick(p0: MenuItem?): Boolean {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun onCreateContextMenu(
-            p0: ContextMenu?,
-            p1: View?,
-            p2: ContextMenu.ContextMenuInfo?
-        ) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun onClick(p0: View?) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-    }
-
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
         container.removeView(`object` as RelativeLayout)
