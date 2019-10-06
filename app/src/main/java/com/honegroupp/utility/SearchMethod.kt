499bb6c80fce9f5d7bcb96b0b7c48dd50841bcae
MCommandar <1686221182@qq.com>
2019-10-06 20:49:36 +1100

    get category ifo

--- app/src/main/java/com/honegroupp/utility/SearchMethod.kt
+++ app/src/main/java/com/honegroupp/utility/SearchMethod.kt
@@ -18,21 +18,22 @@ class SearchMethod{
     /**
      * This function used to create initial view for search activity
      * */
-    fun init(mActivity: AppCompatActivity, listView: ListView, uid: String){
+    fun init(mActivity: AppCompatActivity, listView: ListView, uid: String, category: Int){
         //get whole snapshot of database
         val rootPath = "/"
 
         //get datasnapshot and create view in callback function
         FirebaseDatabaseManager.retrieve(
             rootPath
-        ) { d: DataSnapshot -> callbackInit(mActivity, uid, listView, d) }
+        ) { d: DataSnapshot -> callbackInit(mActivity, uid, category, listView, d) }
     }
 
     /**
      * This callback function is the help function to create search view*/
-    private fun callbackInit(mActivity: AppCompatActivity, uid:String, listView: ListView, dataSnapshot: DataSnapshot){
+    private fun callbackInit(mActivity: AppCompatActivity, uid:String, category: Int, listView: ListView, dataSnapshot: DataSnapshot){
 
         var itemList: ArrayList<Item> = ArrayList()
+        var itemCategoryList: ArrayList<Item> = ArrayList()
 
         // get user's family ID
         val currFamilyId =
@@ -53,6 +54,31 @@ class SearchMethod{
             )
         }
 
+        if (category >= DetailSlide.CATEGORY_SIGNAL){
+            val itemsKey = dataSnapshot.child(FirebaseDatabaseManager.FAMILY_PATH)
+                .child(currFamilyId)
+                .child("category")
+                .child(category.toString())
+                .child("itemKeys")
+
+            for (keyDataSnapshot in itemsKey.children){
+                val key = keyDataSnapshot.getValue(
+                    String::class.java
+                )!!
+
+                for (item in itemList){
+                    if (key == item.key){
+                        itemCategoryList.add(item)
+                    }
+                }
+            }
+        }
+        else if (category == DetailSlide.ALL_PAGE_SIGNAL){
+            itemCategoryList.addAll(itemList)
+        }
+
+
+
         //set adapter to show items
         listView.adapter = ListViewAapter(itemList, mActivity)
 
@@ -60,7 +86,7 @@ class SearchMethod{
             val intent = Intent(mActivity, DetailSlide::class.java)
             intent.putExtra("UserID", uid)
             intent.putExtra("PositionList", listView.adapter.getItemId(position).toString())
-            intent.putExtra("CategoryNameList", "1")
+            intent.putExtra("CategoryNameList", category.toString())
             intent.putExtra("FamilyId", currFamilyId)
             mActivity.startActivity(intent)
         }
@@ -69,19 +95,19 @@ class SearchMethod{
     /**
      * This function used to create initial view for search activity
      * */
-    fun doSearch(mActivity: AppCompatActivity, listView: ListView, uid: String, queryText: String){
+    fun doSearch(mActivity: AppCompatActivity, listView: ListView, uid: String, category: Int, queryText: String){
         //get whole snapshot of database
         val rootPath = "/"
 
         //get datasnapshot and create view in callback function
         FirebaseDatabaseManager.retrieve(
             rootPath
-        ) { d: DataSnapshot -> callbackDoSearch(mActivity, uid, queryText, listView, d) }
+        ) { d: DataSnapshot -> callbackDoSearch(mActivity, uid, category,queryText, listView, d) }
     }
 
     /**
      * This function is the help function to show search view with query*/
-    private fun callbackDoSearch(mActivity: AppCompatActivity, uid: String, queryText: String, listView: ListView, dataSnapshot: DataSnapshot) {
+    private fun callbackDoSearch(mActivity: AppCompatActivity, uid: String, category: Int, queryText: String, listView: ListView, dataSnapshot: DataSnapshot) {
 
         var itemList: ArrayList<Item> = ArrayList()
         var newList: ArrayList<Item> = ArrayList()
@@ -119,12 +145,11 @@ class SearchMethod{
 
 
         listView.setOnItemClickListener { parent, view, position, id ->
-            Toast.makeText(mActivity,position.toString(),Toast.LENGTH_SHORT)
             val element = listView.adapter.getItem(listView.adapter.getItemId(position).toInt()) // The item that was clicked
             val intent = Intent(mActivity, DetailSlide::class.java)
             intent.putExtra("UserID", uid)
             intent.putExtra("PositionList", itemPositionMap[element].toString())
-            intent.putExtra("CategoryNameList", "1")
+            intent.putExtra("CategoryNameList", category.toString())
             intent.putExtra("FamilyId", currFamilyId)
             mActivity.startActivity(intent)
         }
